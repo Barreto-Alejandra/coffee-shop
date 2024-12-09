f39886c80132dd631dc2d20a3bea4d045ae5cb98
Alejandra <56239145+Barreto-Alejandra@users.noreply.github.com>
2024-12-09 19:43:42 +0000

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+import "../css/style.sass";
+import "../css/main.scss";
+
+// import gsap from "gsap";
+// import ScrollTrigger from "gsap/ScrollTrigger";
+// gsap.registerPlugin(ScrollTrigger);
+
+// import Swiper from "swiper";
+
+
+// functions
+
+class Header {
+  constructor() {
+   
+    this.start();
+  }
+  start() {
+    if (this.header) {
+      
+    }
+  }
+}
+
+
+// Run apps
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  new Header();
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./html/index.html"],
+  
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
+  /// Config your own theme https://tailwindcss.com/docs/adding-custom-styles
